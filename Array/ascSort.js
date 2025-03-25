2d4c95f24ac9f71353b0530e3a1b7f19392f2f37
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-25 18:07:37 +0200

    ascSort

---
+++ Array/ascSort.js
@@ -0,0 +1,11 @@
+// Sort an array in ascending order without using .sort()
+
+let arr = [1, 4, 5, 3, 9, 7, 6, 8, 3, 4];
+
+let asc = [];
+
+for (let i = 0; i < arr.length; i++) {
+  asc += arr[i];
+}
+
+console.log(asc);

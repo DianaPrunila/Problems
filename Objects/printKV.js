34e2b1708497c4016a1746a7c05ec05f801a0ac4
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-25 18:08:46 +0200

    printKV

---
+++ Objects/printKV.js
@@ -0,0 +1,7 @@
+// Loop through an object and print all its keys and values
+
+const fName = { a: "ana", b: "bianca", c: "cristina" };
+
+for (let i in fName) {
+  console.log(i, " =", fName[i]);
+}

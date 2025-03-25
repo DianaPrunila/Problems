2d55e312bc82807625618c7a064240a265f531d9
DianaPrunila <pruniladiana03@yahoo.it>
2025-03-25 18:08:19 +0200

    consObj

---
+++ Objects/concObj.js
@@ -0,0 +1,7 @@
+// Merge two objects into one
+const fName = { a: "ana", b: "bianca", c: "cristina" };
+const lName = { x: "anronic", y: "mereuta", z: "mancric" };
+
+let conc = Object.assign(fName, lName);
+
+console.log(conc);
